b8791d0c043c5e69b000fd4778e84f7524722a72
Anna <kochurovaav@gmail.com>
2023-02-25 17:29:52 +0200

    Task0202 is added

---
+++ Task0202/Program.cs
@@ -0,0 +1,12 @@
+﻿ int[,] ChangeArray(int[,] array)
+{
+int[,] result = new int[array.GetLength(0), array.GetLength(1)];
+for (int i = 0; i < array.GetLength(0); i++)
+{
+for (int j = 0; j < array.GetLength(1); j++)
+{
+result[j, i] = array[i, j];
+}
+}
+return result;
+}
